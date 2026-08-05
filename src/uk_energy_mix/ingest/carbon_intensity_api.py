3081b142d7d6bbe6093895ef6ffab63c04b27d9b
Arthur Jones <283991589+ajdjones4@users.noreply.github.com>
2026-08-05 22:01:11 +0100

    Added comments to carbon_intensity_api.py

--- src/uk_energy_mix/ingest/carbon_intensity_api.py
+++ src/uk_energy_mix/ingest/carbon_intensity_api.py
@@ -2,12 +2,17 @@ from pathlib import Path
 import requests
 import json
 
+#Where do we want the raw data to go
 filepath = Path(__file__).parent.parent.parent.parent / "data" / "raw" / "carbon.json"
+
+#what do we want from the API
 header = {
     "Accept": "application/json"
 }
 
+#Make the request to the API
 response = requests.get("https://api.carbonintensity.org.uk/intensity/date", headers=header)
 
+#Write the json response to a file in /data/raw
 with open(filepath, 'w', encoding='utf-8') as file:
     json.dump(response.json(), file, ensure_ascii=False, indent=4)

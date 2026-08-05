442a382ac96250e0d8c076b6b0e35a0966e2c7ca
Arthur Jones <283991589+ajdjones4@users.noreply.github.com>
2026-08-05 21:58:45 +0100

    API script queries for carbon intensity from last 24 hours, outputs a .json to /data/raw

--- src/energy/ingest/carbon-intensity-api.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-headers = {
-    'Accept': 'application/json'
-}
-
-r  = requests.get('https://api.carbonintensity.org.uk/intensity', params={}, headers = headers)
-
-print(r.json())
--- src/uk_energy_mix/ingest/carbon_intensity_api.py
+++ src/uk_energy_mix/ingest/carbon_intensity_api.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 import requests
+import json
 
+filepath = Path(__file__).parent.parent.parent.parent / "data" / "raw" / "carbon.json"
 header = {
     "Accept": "application/json"
 }
 
 response = requests.get("https://api.carbonintensity.org.uk/intensity/date", headers=header)
 
-print(response.json())
+with open(filepath, 'w', encoding='utf-8') as file:
+    json.dump(response.json(), file, ensure_ascii=False, indent=4)
